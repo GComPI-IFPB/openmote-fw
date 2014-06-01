4da588043a5c5796a27b924146b639700dd1b5c4
Pere Tuset <peretuset@gmail.com>
2014-06-01 17:39:57 +0200

    Added the I2C and SPI classes.

---
+++ library/inc/I2c.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2013 OpenMote Technologies, S.L.
+ */
+
+/**
+ *
+ * @file       I2c.h
+ * @author     Pere Tuset-Peiro (peretuset@openmote.com)
+ * @version    v0.1
+ * @date       May, 2014
+ * @brief
+ * @ingroup
+ *
+ */
+
+#ifndef I2C_H_
+#define I2C_H_
+
+#include "InterruptHandler.h"
+
+class I2c
+{
+private:
+
+};
+
+#endif /* I2C_H_ */
---
+++ library/inc/Spi.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2013 OpenMote Technologies, S.L.
+ */
+
+/**
+ *
+ * @file       Spi.h
+ * @author     Pere Tuset-Peiro (peretuset@openmote.com)
+ * @version    v0.1
+ * @date       May, 2014
+ * @brief
+ * @ingroup
+ *
+ */
+
+#ifndef SPI_H_
+#define SPI_H_
+
+#include "InterruptHandler.h"
+
+class Spi
+{
+private:
+
+};
+
+#endif /* SPI_H_ */
