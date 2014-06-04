d343cff36a440ce001631b631aca087e1c698fa5
Pere Tuset <peretuset@gmail.com>
2014-06-05 00:37:20 +0200

    Updated the I2C driver.

--- library/inc/I2c.h
+++ library/inc/I2c.h
@@ -16,12 +16,34 @@
 #ifndef I2C_H_
 #define I2C_H_
 
-#include "InterruptHandler.h"
+#include "interrupt.h"
+#include "ioc.h"
+#include "i2c.h"
+#include "sys_ctrl.h"
+
+#include "hw_ioc.h"
+#include "hw_ints.h"
+#include "hw_types.h"
+#include "hw_i2cm.h"
+#include "hw_i2cs.h"
+
+class Gpio;
 
 class I2c
 {
+public:
+    I2c(uint32_t peripheral_, Gpio* scl_, Gpio* sda_);
+    void init(uint32_t clock_);
+    uint8_t readByte(uint8_t address_, uint8_t register_);
+    uint8_t readByte(uint8_t address_, uint8_t register_, uint8_t * buffer, uint8_t size);
+    void writeByte(uint8_t address_, uint8_t register_, uint8_t data_);
+    void interruptEnable(void);
+    void interruptDisable(void);
+    void interruptHandler(void);
 private:
-
+    uint32_t peripheral;
+    Gpio* scl;
+    Gpio* sda;
 };
 
 #endif /* I2C_H_ */
--- library/src/I2c.cpp
+++ library/src/I2c.cpp
@@ -1,1 +1,79 @@
 #include "I2c.h"
+#include "Gpio.h"
+#include "InterruptHandler.h"
+
+I2c::I2c(uint32_t peripheral_, Gpio* scl_, Gpio* sda_):
+    peripheral(peripheral_), scl(scl_), sda(sda_)
+{
+    SysCtrlPeripheralEnable(peripheral);
+    SysCtrlPeripheralReset(peripheral);
+
+    GPIOPinTypeI2C(scl->getPort(), scl->getPin());
+    GPIOPinTypeI2C(sda->getPort(), sda->getPin());
+
+    IOCPinConfigPeriphInput(scl->getPort(), scl->getPin(), IOC_I2CMSSCL);
+    IOCPinConfigPeriphInput(sda->getPort(), sda->getPin(), IOC_I2CMSSDA);
+    IOCPinConfigPeriphOutput(scl->getPort(), scl->getPin(), IOC_MUX_OUT_SEL_I2C_CMSSCL);
+    IOCPinConfigPeriphOutput(sda->getPort(), sda->getPin(), IOC_MUX_OUT_SEL_I2C_CMSSDA);
+}
+
+void I2c::init(uint32_t clock_)
+{
+    bool status;
+
+    status = (clock_ == 400000 ? true : false);
+    
+    I2CMasterEnable();
+
+    I2CMasterInitExpClk(SysCtrlClockGet(), status);
+}
+
+uint8_t I2c::readByte(uint8_t address_, uint8_t register_)
+{
+    uint8_t data;
+
+    I2CMasterSlaveAddrSet(address_, false);
+
+    I2CMasterDataPut(register_);
+
+    I2CMasterControl(I2C_MASTER_CMD_SINGLE_SEND);
+
+    while(I2CMasterBusy())
+        ;
+
+    data = I2CMasterDataGet();
+
+    I2CMasterSlaveAddrSet(address_, true);
+
+    I2CMasterDataPut(register_);
+
+    I2CMasterControl(I2C_MASTER_CMD_SINGLE_SEND);
+
+    while(I2CMasterBusy())
+        ;
+
+    data = I2CMasterDataGet();
+    
+    return data;
+}
+
+uint8_t I2c::readByte(uint8_t address_, uint8_t register_, uint8_t * buffer, uint8_t size)
+{
+    return 0;
+}
+
+void I2c::writeByte(uint8_t address_, uint8_t register_, uint8_t data_)
+{
+}
+
+void I2c::interruptEnable(void)
+{
+}
+
+void I2c::interruptDisable(void)
+{
+}
+
+void I2c::interruptHandler(void)
+{
+}
