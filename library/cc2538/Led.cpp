71b080f5b110a3f571c18cdf054143703b6a6761
Pere Tuset <peretuset@gmail.com>
2014-06-22 22:11:12 +0200

    Updated the Led class.

--- library/cc2538/Led.cpp
+++ library/cc2538/Led.cpp
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2013 OpenMote Technologies, S.L.
+ */
+
+/**
+ *
+ * @file       Gpio.cpp
+ * @author     Pere Tuset-Peiro (peretuset@openmote.com)
+ * @version    v0.1
+ * @date       May, 2014
+ * @brief
+ * @ingroup
+ *
+ */
+
 #include "Led.h"
 
 Led::Led(uint32_t port_, uint8_t pin_):
