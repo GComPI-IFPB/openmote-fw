1f86e2a83cfb74027b8dac7771eed9909f8bd429
Pere Tuset <peretuset@gmail.com>
2014-07-13 20:30:59 +0200

    Updated the SHT21 driver.

--- sensors/sht21/Sht21.cpp
+++ sensors/sht21/Sht21.cpp
@@ -22,18 +22,31 @@
 
 #define SHT21_ADDRESS                   ( 0x40 )
 
-#define SHT21_WAKEUP_TIME_US            ( 15000 )
+#define SHT21_USER_REG_READ             ( 0xE7 )
+#define SHT21_USER_REG_WRITE            ( 0xE6 )
+#define SHT21_USER_REG_RESERVED_BITS    ( 0x38 )
 
-#define SHT21_CONFIG_DEFAULT            ( 0x3A )
-
-#define SHT21_READ_USER_REG             ( 0xE7 )
-#define SHT21_WRITE_USER_REG            ( 0xE6 )
+#define SHT21_RESOLUTION_12b_14b        ( (0 << 7) | (0 << 0) ) // 29 + 85 = 114 ms
+#define SHT21_RESOLUTION_8b_12b         ( (0 << 7) | (1 << 0) ) //  4 + 22 =  26 ms
+#define SHT21_RESOLUTION_10b_13b        ( (1 << 7) | (0 << 0) ) //  9 + 43 =  52 ms
+#define SHT21_RESOLUTION_11b_11b        ( (1 << 7) | (1 << 0) ) // 15 + 11 =  26 ms
+#define SHT21_ONCHIP_HEATER_ENABLE      ( 1 << 2 )
+#define SHT21_ONCHIP_HEATER_DISABLE     ( 0 << 2 )
+#define SHT21_BATTERY_ABOVE_2V25        ( 0 << 6 )
+#define SHT21_BATTERY_BELOW_2V25        ( 1 << 6 )
+#define SHT21_OTP_RELOAD_ENABLE         ( 0 << 1 )
+#define SHT21_OTP_RELOAD_DISABLE        ( 1 << 1 )
 
 #define SHT21_TEMPERATURE_HM_CMD        ( 0xE3 )
 #define SHT21_HUMIDITY_HM_CMD           ( 0xE5 )
 #define SHT21_TEMPERATURE_NHM_CMD       ( 0xF3 )
 #define SHT21_HUMIDITY_NHM_CMD          ( 0xF5 )
 #define SHT21_RESET_CMD                 ( 0xFE )
+
+#define SHT21_DEFAULT_CONFIG            ( SHT21_RESOLUTION_8b_12b | \
+                                          SHT21_ONCHIP_HEATER_DISABLE | \
+                                          SHT21_BATTERY_ABOVE_2V25 \ \
+                                          SHT21_OTP_RELOAD_DISABLE )
 
 /*********************************variables***********************************/
 
@@ -48,7 +61,22 @@ Sht21::Sht21(I2cDriver* i2c_):
 
 bool Sht21::enable(void)
 {
-    return false;
+    bool status;
+    uint8_t config;
+    
+    i2c->lock();
+    
+    status = i2c->writeByte(SHT21_ADDRESS, SHT21_READ_USER_REG);
+    status = i2c->readByte(SHT21_ADDRESS, &config);
+
+    config &= SHT21_USER_REG_RESERVED_BITS;
+    config |= SHT21_DEFAULT_CONFIG;
+
+    status = i2c->writeByte(SHT21_ADDRESS, SHT21_WRITE_USER_REG, &config);
+    
+    i2c->unlock();
+
+    return status;
 }
 
 bool Sht21::reset(void)
