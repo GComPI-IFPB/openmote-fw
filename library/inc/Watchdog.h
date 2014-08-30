66d6d92caa4744362d994600da99b0c094f9c493
Pere Tuset <peretuset@gmail.com>
2014-08-30 18:54:40 +0200

    Added the Watchdog class.

---
+++ library/inc/Watchdog.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2013 OpenMote Technologies, S.L.
+ */
+
+/**
+ *
+ * @file       Watchdog.h
+ * @author     Pere Tuset-Peiro (peretuset@openmote.com)
+ * @version    v0.1
+ * @date       May, 2014
+ * @brief
+ * @ingroup
+ *
+ */
+
+#ifndef WATCHDOG_H_
+#define WATCHDOG_H_
+
+#include <stdint.h>
+
+class Watchdog
+{
+public:
+    Watchdog();
+};
+
+#endif /* WATCHDOG_H_ */
