dee8414431e06c607c30049a3d2bae7952fbcdb2
Pere Tuset <peretuset@gmail.com>
2014-08-31 12:10:58 +0200

    Added the test-serial project.

---
+++ projects/test-serial/main.cpp
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2013 OpenMote Technologies, S.L.
+ */
+
+/**
+ *
+ * @file       main.cpp
+ * @author     Pere Tuset-Peiro (peretuset@openmote.com)
+ * @version    v0.1
+ * @date       May, 2014
+ * @brief
+ * @ingroup
+ *
+ */
+
+/*================================ include ==================================*/
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+
+#include "openmote-cc2538.h"
+
+#include "Serial.h"
+
+/*================================ define ===================================*/
+
+#define GREEN_LED_TASK_PRIORITY             ( tskIDLE_PRIORITY + 1 )
+#define SERIAL_TASK_PRIORITY                ( tskIDLE_PRIORITY + 0 )
+
+/*================================ typedef ==================================*/
+
+/*=============================== prototypes ================================*/
+
+static void prvGreenLedTask(void *pvParameters);
+static void prvSerialTask(void *pvParameters);
+
+/*=============================== variables =================================*/
+
+uint8_t serial_tx_buffer[] = {'O','p','e','n','M','o','t','e','-','C','C','2','5','3','8'};
+uint8_t* serial_tx_ptr = serial_tx_buffer;
+uint8_t serial_tx_len  = sizeof(serial_tx_buffer);
+
+uint8_t serial_rx_buffer[128];
+uint8_t* serial_rx_ptr = serial_rx_buffer;
+uint8_t serial_rx_len  = sizeof(serial_rx_buffer);
+
+static Serial serial(uart);
+
+/*================================= public ==================================*/
+
+int main (void)
+{
+    // Set the TPS62730 in bypass mode (Vin = 3.3V, Iq < 1 uA)
+    tps62730.setBypass();
+
+    // Enable the UART peripheral and the serial driver
+    uart.enable(UART_BAUDRATE, UART_CONFIG, UART_INT_MODE);
+    serial.enable();
+
+    spi.enable(SPI_MODE, SPI_PROTOCOL, SPI_DATAWIDTH, SPI_BAUDRATE);
+
+    // Create two FreeRTOS tasks
+    xTaskCreate(prvGreenLedTask, (const char *) "Green", 128, NULL, GREEN_LED_TASK_PRIORITY, NULL);
+    xTaskCreate(prvSerialTask, (const char *) "Serial", 128, NULL, SERIAL_TASK_PRIORITY, NULL);
+
+    // Kick the FreeRTOS scheduler
+    vTaskStartScheduler();
+}
+
+/*=============================== protected =================================*/
+
+/*================================ private ==================================*/
+
+static void prvSerialTask(void *pvParameters)
+{
+    // Turn on red LED
+    led_red.on();
+
+    // Print buffer via Serial/UART
+    serial.printf(serial_tx_ptr, serial_tx_len);
+
+    // Turn off red LED
+    led_red.off();
+
+    // Forever
+    while (true)
+    {
+        serial_rx_ptr = serial_rx_buffer;
+        serial_rx_len = sizeof(serial_rx_buffer);
+
+        // Read buffer via Serial/UART
+        serial_rx_len = serial.scanf(serial_rx_ptr, serial_rx_len);
+
+        // Delay for 250 ms
+        vTaskDelay(250 / portTICK_RATE_MS);
+
+        // Turn on red LED
+        led_red.on();
+
+        // Write buffer via Serial/UART
+        serial.printf(serial_rx_ptr, serial_rx_len);
+
+        // Turn off red LED
+        led_red.off();
+    }
+}
+
+static void prvGreenLedTask(void *pvParameters)
+{
+    // Forever
+    while (true)
+    {
+        // Turn off green LED for 950 ms
+        led_green.off();
+        vTaskDelay(950 / portTICK_RATE_MS);
+
+        // Turn on green LED for 50 ms
+        led_green.on();
+        vTaskDelay(50 / portTICK_RATE_MS);
+    }
+}
