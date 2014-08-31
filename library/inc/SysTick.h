5747437d73f78a8ce21302eae296b11b7ed95cc5
Pere Tuset <peretuset@gmail.com>
2014-08-31 09:27:03 +0200

    Added the SysTick class.

---
+++ library/inc/SysTick.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2013 OpenMote Technologies, S.L.
+ */
+
+/**
+ *
+ * @file       SysTick.h
+ * @author     Pere Tuset-Peiro (peretuset@openmote.com)
+ * @version    v0.1
+ * @date       May, 2014
+ * @brief
+ * @ingroup
+ *
+ */
+
+#ifndef SYSTICK_H_
+#define SYSTICK_H_
+
+#include <stdint.h>
+
+class SysTick
+{
+public:
+    SysTick();
+private:
+};
+
+#endif /* SYSTICK_H_ */
