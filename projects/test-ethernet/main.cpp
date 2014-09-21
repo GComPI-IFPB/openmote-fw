c8e3126cf78ee73a91e4ae97747642a458498ec2
Pere Tuset <peretuset@gmail.com>
2014-09-22 01:19:14 +0200

    Updated the test-ethernet project.

--- projects/test-ethernet/main.cpp
+++ projects/test-ethernet/main.cpp
@@ -37,18 +37,21 @@ static void prvEthernetTask(void *pvParameters);
 
 /*=============================== variables =================================*/
 
-uint8_t eui64_address[8];
 uint8_t eui48_address[6];
-uint8_t ethernet_payload[60] = {
-                            0xA4, 0x5D, 0x36, 0x5B, 0xA4, 0xD0,
-                            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-                            0x08, 0x06,
-                            0x00, 0x01, 0x08, 0x06, 0x04, 0x00, 0x02, 0x00,
-                            0x00, 0x01, 0x08, 0x06, 0x04, 0x00, 0x02, 0x00,
-                            0x00, 0x01, 0x08, 0x06, 0x04, 0x00, 0x02, 0x00,
-                            0x00, 0x01, 0x08, 0x06, 0x04, 0x00, 0x02, 0x00,
-                            0x00, 0x01, 0x08, 0x06, 0x04, 0x00, 0x02, 0x00,
-                            0x00, 0x01, 0x08, 0x06, 0x04, 0x00};
+
+uint8_t tx_frame[] =
+                    { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+                      0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+                      0x80, 0x00,
+                      0x53, 0x68, 0x69, 0x6E, 0x69, 0x6E, 0x67, 0x20, 0x42, 0x69,
+                      0x74, 0x73, 0x2C, 0x20, 0x65, 0x6D, 0x62, 0x65, 0x64, 0x64,
+                      0x65, 0x64, 0x20, 0x69, 0x6E, 0x74, 0x65, 0x6C, 0x6C, 0x69,
+                      0x67, 0x65, 0x6E, 0x63, 0x65, 0x20, 0x21 };
+
+uint8_t* tx_frame_ptr = tx_frame;
+uint32_t tx_frame_len = sizeof(tx_frame);
+
+Ethernet ethernet(enc28j60);
 
 /*================================= public ==================================*/
 
@@ -74,17 +77,14 @@ int main (void)
 
 static void prvEthernetTask(void *pvParameters)
 {
-    // Get the EUI-64 address
-    board.getAddress(eui64_address);
+    // Get EUI-48 address
+    board.getEUI48(eui48_address);
 
-    // Set the EUI-48 address
-    memcpy(&eui48_address[0], &eui64_address[2], 6);
-
-    // Set the EUI-48 address to the packet
-    memcpy(&ethernet_payload[6], eui48_address, 6);
+    // Set the EUI-48 address to the transmit frame
+    memcpy(&tx_frame[6], eui48_address, 6);
 
     // Use the EUI-48 address as the MAC address
-    enc28j60.init(eui48_address);
+    ethernet.init(eui48_address);
 
     // Forever
     while (true)
@@ -92,14 +92,8 @@ static void prvEthernetTask(void *pvParameters)
         // Transmit a packet every second
         vTaskDelay(1000 / portTICK_RATE_MS);
 
-        // Turn on red LED
-        led_red.on();
-
         // Send Ethernet payload
-        enc28j60.transmitFrame(ethernet_payload, sizeof(ethernet_payload));
-
-        // Turn off red LED
-        led_red.off();
+        ethernet.transmitFrame(tx_frame_ptr, tx_frame_len);
     }
 }
 
