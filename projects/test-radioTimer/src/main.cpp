e9a7faaa8cd521e5c086553b6c9aa7a1266aed14
Pere Tuset <peretuset@gmail.com>
2015-01-26 08:05:28 +0100

    Updated the radioTimer test project.

--- projects/test-radioTimer/src/main.cpp
+++ projects/test-radioTimer/src/main.cpp
@@ -26,8 +26,12 @@
 /*=============================== prototypes ================================*/
 
 static void sleep_timer_callback(void);
+static void radio_timer_period_callback(void);
+static void radio_timer_compare_callback(void);
 
 static PlainCallback sleepTimerCallback(sleep_timer_callback);
+static PlainCallback radioTimerPeriodCallback(radio_timer_period_callback);
+static PlainCallback radioTimerCompareCallback(radio_timer_compare_callback);
 
 /*=============================== variables =================================*/
 
@@ -38,13 +42,22 @@ int main (void)
     // Enable erasing the Flash with the user button
     board.enableFlashErase();
 
-    // Initialize Rtc
-    sleepTimer.enable();
+    // Initialize sleepTimer
     sleepTimer.setCallback(&sleepTimerCallback);
     sleepTimer.enableInterrupts();
 
-    // Start Rtc
+    // Initialize radioTimer
+    radioTimer.setPeriodCallback(&radioTimerPeriodCallback);
+    radioTimer.setCompareCallback(&radioTimerCompareCallback);
+    radioTimer.enableInterrupts();
+
+    // Start sleepTimer
     sleepTimer.start(32768);
+
+    // Start radioTimer
+    radioTimer.setPeriod(8192);
+    radioTimer.setCompare(4196);
+    radioTimer.start();
 
     // Enable interrupts
     board.enableInterrupts();
@@ -68,14 +81,45 @@ static void sleep_timer_callback(void)
 
     if (led_status)
     {
-        led_status = false;
         led_green.off();
         sleepTimer.start(32768);
     }
     else
     {
-        led_status = true;
         led_green.on();
         sleepTimer.start(327);
     }
+
+    led_status = !led_status;
+}
+
+static void radio_timer_period_callback(void)
+{
+    static bool led_status = false;
+
+    if (led_status)
+    {
+        led_orange.off();
+    }
+    else
+    {
+        led_orange.on();
+    }
+
+    led_status = !led_status;
+}
+
+static void radio_timer_compare_callback(void)
+{
+    static bool led_status = false;
+
+    if (led_status)
+    {
+        led_yellow.off();
+    }
+    else
+    {
+        led_yellow.on();
+    }
+    led_status = !led_status;
 }
