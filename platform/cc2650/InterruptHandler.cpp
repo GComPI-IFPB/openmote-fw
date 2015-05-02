05ab0c7604cba333acc2335bf815bf25e62bf2d3
Pere Tuset <peretuset@gmail.com>
2015-05-02 22:53:47 +0200

    Added initial support for the CC2650 platform.

---
+++ platform/cc2650/InterruptHandler.cpp
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2013 OpenMote Technologies, S.L.
+ */
+
+/**
+ *
+ * @file       InterruptHandler.cpp
+ * @author     Pere Tuset-Peiro (peretuset@openmote.com)
+ * @version    v0.1
+ * @date       May, 2014
+ * @brief
+ * @ingroup
+ *
+ */
+
+/*================================ include ==================================*/
+
+#include "InterruptHandler.h"
+
+#include "GpioIn.h"
+#include "GpioInPow.h"
+#include "Timer.h"
+#include "Uart.h"
+#include "I2c.h"
+#include "Spi.h"
+#include "Radio.h"
+#include "SleepTimer.h"
+#include "RadioTimer.h"
+#include "SysTick.h"
+
+/*================================ define ===================================*/
+
+/*================================ typedef ==================================*/
+
+/*=============================== variables =================================*/
+
+/*=============================== prototypes ================================*/
+
+/*================================= public ==================================*/
+
+/*=============================== protected =================================*/
+
+/*================================ private ==================================*/
+
