b5a61ec9eed63821d7394a2a3996a8d8df27ce8f
Pere Tuset <peretuset@gmail.com>
2016-07-03 16:39:02 +0200

    Add Adc and Sha256 interface and empty implementation.

---
+++ platform/inc/Sha256.h
@@ -0,0 +1,19 @@
+/**
+ * @file       Sha256.h
+ * @author     Pere Tuset-Peiro (peretuset@openmote.com)
+ * @version    v0.1
+ * @date       May, 2016
+ * @brief
+ *
+ * @copyright  Copyright 2016, OpenMote Technologies, S.L.
+ *             This file is licensed under the GNU General Public License v2.
+ */
+
+#ifndef SHA_256_H_
+#define SHA_256_H_
+
+class Sha256 {
+
+};
+
+#endif /* SHA_256_H_ */
