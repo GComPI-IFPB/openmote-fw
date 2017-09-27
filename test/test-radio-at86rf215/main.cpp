a4bf4551a35d3d1d7c3f2931503cc32fca79dfb2
Pere Tuset <peretuset@gmail.com>
2017-09-28 00:44:24 +0200

    Updated test project for Atmel AT86RF215 radio.

--- test/test-radio-at86rf215/main.cpp
+++ test/test-radio-at86rf215/main.cpp
@@ -33,30 +33,70 @@
 
 /*=============================== prototypes ================================*/
 
+static void rf09Callback(void);
+static void rf24Callback(void);
+
 /*=============================== variables =================================*/
+
+static PlainCallback rf09Callback_(rf09Callback);
+static PlainCallback rf24Callback_(rf24Callback);
 
 /*================================= public ==================================*/
 
 int main(void)
 {
+    bool status;
+
 	// Initialize board
 	board.init();
 
     // Enable the SPI peripheral
     spi.enable(SPI_BAUDRATE);
 
-    // Put CC1200 in transmit mode
-    at86rf215.on();
+    // Set radio callbacks
+    at86rf215.setCallbacks(&rf09Callback_, &rf24Callback_);
+    at86rf215.enableInterrupts();
 
-	if(at86rf215.check()) {
-        led_green.on();
-    } else {
-        led_red.on();
-    }
+    // Enable general interrupts
+    board.enableInterrupts();
 
     // Forever and ever!
-    while(true)
-    	;
+    while (true) {
+        
+        at86rf215.enable();
+
+        board.delayMilliseconds(1000);
+        
+        status = at86rf215.check();
+
+        if (status) {
+            led_green.on();
+        } else {
+            led_red.on();
+        }
+
+        board.delayMilliseconds(1000);
+
+        at86rf215.sleep();
+
+        led_green.off();
+        led_orange.off();
+        led_yellow.off();
+        led_red.off();
+
+        board.delayMilliseconds(1000);
+    }
+    	
 }
 
 /*================================ private ==================================*/
+
+static void rf09Callback(void)
+{
+    led_yellow.on();
+}
+
+static void rf24Callback(void)
+{
+    led_orange.on();
+}
