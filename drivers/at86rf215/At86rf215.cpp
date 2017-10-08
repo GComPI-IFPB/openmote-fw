c7d2b074c8704e4b909e5c8daf03462f6206dcf3
Pere Tuset <peretuset@gmail.com>
2017-10-08 21:57:30 +0200

    Work in progress for AT86RF215 driver.

--- drivers/at86rf215/At86rf215.cpp
+++ drivers/at86rf215/At86rf215.cpp
@@ -26,6 +26,9 @@
 
 extern Board board;
 
+uint8_t rf09_irqm, rf24_irqm;
+uint8_t bbc0_irqm, bbc1_irqm;
+
 /*=============================== prototypes ================================*/
 
 /*================================= public ==================================*/
@@ -38,29 +41,70 @@ At86rf215::At86rf215(Spi& spi, GpioOut& pwr, GpioOut& rst, GpioOut& cs, GpioIn& 
     irq_.setCallback(&callback_);
 }
 
-void At86rf215::enable(void)
+void At86rf215::on(void)
 {
     pwr_.on();
     board.delayMilliseconds(1);
     rst_.low();
 }
 
-void At86rf215::sleep(void)
+void At86rf215::off(void)
 {
     rst_.high();
     board.delayMilliseconds(1);
     pwr_.off();
 }
 
-void At86rf215::wakeup(void)
-{
-}
-
-void At86rf215::reset(void)
+void At86rf215::hardReset(void)
 {
     rst_.high();
     board.delayMilliseconds(1);
     rst_.low();
+}
+
+void At86rf215::softReset(RadioCore rc)
+{
+	goToState(rc, RadioCommand::CMD_RESET, RadioState::STATE_RESET);
+}
+
+void At86rf215::configure(void)
+{
+	rf09_irqm = 0x00;
+    singleAccessWrite(AT86RF215_RF09_IRQM_ADDR, rf09_irqm);
+
+    rf24_irqm = 0x00;
+    singleAccessWrite(AT86RF215_RF24_IRQM_ADDR, rf24_irqm);
+
+    bbc0_irqm = AT86RF215_BBCn_IRQM_TXFE_(1) | AT86RF215_BBCn_IRQM_RXFE_(1) | AT86RF215_BBCn_IRQM_RXFS_(1);
+    singleAccessWrite(AT86RF215_BBC0_IRQM_ADDR, bbc0_irqm);
+
+    bbc1_irqm = AT86RF215_BBCn_IRQM_TXFE_(1) | AT86RF215_BBCn_IRQM_RXFE_(1) | AT86RF215_BBCn_IRQM_RXFS_(1);
+    singleAccessWrite(AT86RF215_BBC1_IRQM_ADDR, bbc1_irqm);
+}
+
+void At86rf215::sleep(RadioCore rc)
+{
+	goToState(rc, RadioCommand::CMD_SLEEP, RadioState::STATE_SLEEP);
+}
+
+void At86rf215::wakeup(RadioCore rc)
+{
+	goToState(rc, RadioCommand::CMD_TRXOFF, RadioState::STATE_TRXOFF);
+}
+
+void At86rf215::ready(RadioCore rc)
+{
+	goToState(rc, RadioCommand::CMD_TXPREP, RadioState::STATE_TXPREP);
+}
+
+void At86rf215::transmit(RadioCore rc)
+{
+	goToState(rc, RadioCommand::CMD_TX, RadioState::STATE_TX);
+}
+
+void At86rf215::receive(RadioCore rc)
+{
+	goToState(rc, RadioCommand::CMD_RX, RadioState::STATE_RX);
 }
 
 bool At86rf215::check(void)
@@ -68,14 +112,14 @@ bool At86rf215::check(void)
     uint8_t pn;
     uint8_t ver;
 
-    singleAccessRead(AT86RF215_RF_PN_ADDR, &pn);
+    singleAccessRead(AT86RF215_PN_ADDR, &pn);
 
-    if (pn == AT86RF215_RF_PN_215) {
+    if (pn == AT86RF215_PN_215) {
         
-        singleAccessRead(AT86RF215_RF_VN_ADDR, &ver);
+        singleAccessRead(AT86RF215_VN_ADDR, &ver);
 
-        if (ver == AT86RF215_RF_VN_1_1 ||
-            ver == AT86RF215_RF_VN_1_3)
+        if (ver == AT86RF215_VN_1_1 ||
+            ver == AT86RF215_VN_1_3)
         {
             return true;
         }
@@ -91,18 +135,30 @@ bool At86rf215::check(void)
     }
 }
 
-void At86rf215::setCallbacks(Callback* rf09, Callback* rf24)
+void At86rf215::setRxCallbacks(RadioCore rc, Callback* rxInit, Callback* rxDone)
 {
-    /* Store the receive init and done callbacks */
-    rf09_cb = rf09;
-    rf24_cb = rf24;
+   	if (rc == RadioCore::CORE_RF09)
+	{
+		rx09Init_ = rxInit;
+    	rx09Done_ = rxDone;
+	} else if  (rc == RadioCore::CORE_RF24)
+	{
+		rx24Init_ = rxInit;
+    	rx24Done_ = rxDone;
+	}
 }
 
-void At86rf215::clearCallbacks(void)
+void At86rf215::setTxCallbacks(RadioCore rc, Callback* txInit, Callback* txDone)
 {
-    /* Re-store the receive init and done callbacks */
-    rf09_cb = nullptr;
-    rf24_cb = nullptr;
+	if (rc == RadioCore::CORE_RF09)
+	{
+		tx09Init_ = txInit;
+    	tx09Done_ = txDone;
+	} else if  (rc == RadioCore::CORE_RF24)
+	{
+		tx24Init_ = txInit;
+    	tx24Done_ = txDone;
+	}
 }
 
 void At86rf215::enableInterrupts(void)
@@ -115,34 +171,194 @@ void At86rf215::disableInterrupts(void)
     irq_.disableInterrupts();
 }
 
+bool At86rf215::getRSSI(RadioCore rc, int8_t* rssi)
+{
+	int8_t value;
+
+	if (rc == RadioCore::CORE_RF09)
+	{
+		singleAccessRead(AT86RF215_RF09_RSSI_ADDR, (uint8_t *)&value);
+	} else if (rc == RadioCore::CORE_RF24)
+	{
+		singleAccessRead(AT86RF215_RF24_RSSI_ADDR, (uint8_t *)&value);
+	} else {
+		return false;
+	}
+
+	if (value == 127) 
+	{
+		*rssi = 0;
+		return false;
+	}
+	else
+	{
+		*rssi = value;
+		return true;
+	}
+}
+
+bool At86rf215::getED(RadioCore rc, int8_t* ed)
+{
+	int8_t value;
+
+	if (rc == RadioCore::CORE_RF09)
+	{
+		singleAccessRead(AT86RF215_RF09_EDV_ADDR, (uint8_t *)&value);
+	} else if (rc == RadioCore::CORE_RF24)
+	{
+		singleAccessRead(AT86RF215_RF24_EDV_ADDR, (uint8_t *)&value);
+	} else {
+		return false;
+	}
+
+	if (value < -127 || value > 4) 
+	{
+		*ed = 0;
+		return false;
+	}
+	else
+	{
+		*ed = value;
+		return true;
+	}
+}
+
+At86rf215::RadioResult At86rf215::loadPacket(RadioCore rc, uint8_t* data, uint8_t length)
+{
+	return RadioResult::Error;
+}
+
+At86rf215::RadioResult At86rf215::getPacket(RadioCore rc, uint8_t* buffer, uint8_t* length, int8_t* rssi, uint8_t* lqi, uint8_t* crc)
+{
+	return RadioResult::Error;
+}
+
 /*=============================== protected =================================*/
 
 void At86rf215::interruptHandler(void)
 {
     /* Read interrupt registers */
-    uint8_t rf09, rf24;
+    uint8_t rf09_irqs, rf24_irqs;
+    uint8_t bbc0_irqs, bbc1_irqs;
 
-    singleAccessRead(AT86RF215_RF_RFN09_IRQS_ADDR, &rf09);
-    singleAccessRead(AT86RF215_RF_RFN24_IRQS_ADDR, &rf24);
+	/* Read RF and BBC interrupt status */
+    singleAccessRead(AT86RF215_RF09_IRQS_ADDR, &rf09_irqs);
+    singleAccessRead(AT86RF215_RF24_IRQS_ADDR, &rf24_irqs);
+    singleAccessRead(AT86RF215_BBC0_IRQS_ADDR, &bbc0_irqs);
+    singleAccessRead(AT86RF215_BBC1_IRQS_ADDR, &bbc1_irqs);
 
-    if (rf09 & AT86RF215_RF_RFn_IRQS_WAKEUP)
+    /* Call RF09 interrupt handler */
+    if (rf09_irqs & rf09_irqm || 
+    	bbc0_irqs & bbc0_irqm)
     {
-        if (rf09_cb != nullptr)
-        {
-            rf09_cb->execute();
-        }
+    	interruptHandler_rf09(rf09_irqs, bbc0_irqs);
     }
 
-    if (rf24 & AT86RF215_RF_RFn_IRQS_WAKEUP)
+	/* Call RF24 interrupt handler */
+    if (rf24_irqs & rf24_irqm || 
+    	bbc1_irqs & bbc1_irqm)
     {
-        if (rf24_cb != nullptr)
-        {
-            rf24_cb->execute();
-        }
+        interruptHandler_rf24(rf24_irqs, bbc1_irqs);
+    }
+}
+
+void At86rf215::interruptHandler_rf09(uint8_t rf_irqs, uint8_t bbc_irqs)
+{
+	/* Receiver start of frame */
+	if (bbc_irqs & AT86RF215_BBCn_IRQM_RXFS_(1) &&
+		rx09Init_ != nullptr)
+    {
+        rx09Init_->execute();
+    }
+
+	/* Receiver end of frame */
+    if (bbc_irqs & AT86RF215_BBCn_IRQM_RXFE_(1) &&
+    	rx09Done_ != nullptr)
+    {
+        rx09Done_->execute();
+    }
+
+    if (tx09Init_ != nullptr)
+    {
+        // tx09Init_->execute();
+    }
+
+	/* Transmitter end of frame */
+    if (bbc_irqs & AT86RF215_BBCn_IRQM_TXFE_(1) &&
+    	tx09Done_ != nullptr)
+    {
+        tx09Done_->execute();
+    }
+}
+
+void At86rf215::interruptHandler_rf24(uint8_t rf_irqs, uint8_t bbc_irqs)
+{
+	/* Receiver start of frame */
+	if (bbc_irqs & AT86RF215_BBCn_IRQM_RXFS_(1) &&
+		rx24Init_ != nullptr)
+    {
+        rx24Init_->execute();
+    }
+
+	/* Receiver end of frame */
+    if (bbc_irqs & AT86RF215_BBCn_IRQM_RXFE_(1) &&
+    	rx24Done_ != nullptr)
+    {
+        rx24Done_->execute();
+    }
+
+    if (tx24Init_ != nullptr)
+    {
+        // tx24Init_->execute();
+    }
+
+	/* Transmitter end of frame */
+    if (bbc_irqs & AT86RF215_BBCn_IRQM_TXFE_(1) &&
+    	tx24Done_ != nullptr)
+    {
+        tx24Done_->execute();
     }
 }
 
 /*================================ private ==================================*/
+
+At86rf215::RadioState At86rf215::getState(RadioCore rc)
+{
+	RadioState state;
+
+	if (rc == RadioCore::CORE_RF09)
+	{
+		singleAccessRead(AT86RF215_RF09_STATE_ADDR, (uint8_t*) &state);	
+	} else if (rc == RadioCore::CORE_RF24)
+	{
+		singleAccessRead(AT86RF215_RF24_STATE_ADDR, (uint8_t*) &state);	
+	}
+
+	return state;
+}
+
+void At86rf215::goToState(RadioCore rc, RadioCommand cmd, RadioState target)
+{
+	RadioState current;
+
+	writeCmd(rc, cmd);
+
+	do {
+		board.delayMilliseconds(1);
+		current = getState(rc);
+	} while (target != current);
+}
+
+void At86rf215::writeCmd(RadioCore rc, RadioCommand cmd)
+{
+	if (rc == RadioCore::CORE_RF09)
+	{
+		singleAccessWrite(AT86RF215_RF09_CMD_ADDR, (uint8_t) cmd);
+	} else if (rc == RadioCore::CORE_RF24)
+	{
+		singleAccessWrite(AT86RF215_RF24_CMD_ADDR, (uint8_t) cmd);	
+	}
+}
 
 void At86rf215::singleAccessRead(uint16_t address, uint8_t* value)
 {
--- test/test-radio-at86rf215/main.cpp
+++ test/test-radio-at86rf215/main.cpp
@@ -11,92 +11,224 @@
 
 /*================================ include ==================================*/
 
+#include <string.h>
+
 #include "FreeRTOS.h"
 #include "task.h"
+#include "semphr.h"
 
 #include "board.h"
-#include "platform_types.h"
-
-#include "At86rf215.h"
-#include "At86rf215_regs.h"
 
 #include "Callback.h"
 #include "Scheduler.h"
 #include "Semaphore.h"
+#include "Serial.h"
 #include "Task.h"
 
 /*================================ define ===================================*/
 
+#define RADIO_MODE_RX                       ( 0 )
+#define RADIO_MODE_TX                       ( 1 )
+
+#define RADIO_CORE_24G                      ( At86rf215::CORE_RF24 )
+#define RADIO_CORE_SUB                      ( At86rf215::CORE_RF09 )
+
+#define RADIO_MODE                          ( RADIO_MODE_TX )
+#define RADIO_CORE                          ( RADIO_CORE_24G )
+
+#define PAYLOAD_LENGTH                      ( 125 )
+#define EUI48_LENGTH                        ( 6 )
+
+#define UART_BAUDRATE                       ( 115200 )
 #define SPI_BAUDRATE                        ( 8000000 )
+
+#define GREEN_LED_TASK_PRIORITY             ( tskIDLE_PRIORITY + 2 )
+#define RADIO_RX_TASK_PRIORITY              ( tskIDLE_PRIORITY + 0 )
+#define RADIO_TX_TASK_PRIORITY              ( tskIDLE_PRIORITY + 0 )
 
 /*================================ typedef ==================================*/
 
 /*=============================== prototypes ================================*/
 
-static void rf09Callback(void);
-static void rf24Callback(void);
+static void prvGreenLedTask(void *pvParameters);
+static void prvRadioRxTask(void *pvParameters);
+static void prvRadioTxTask(void *pvParameters);
+
+static void rxInit(void);
+static void rxDone(void);
+static void txInit(void);
+static void txDone(void);
 
 /*=============================== variables =================================*/
 
-static PlainCallback rf09Callback_(rf09Callback);
-static PlainCallback rf24Callback_(rf24Callback);
+static SemaphoreBinary rxSemaphore, txSemaphore;
+
+static PlainCallback rxInitCallback(&rxInit);
+static PlainCallback rxDoneCallback(&rxDone);
+static PlainCallback txInitCallback(&txInit);
+static PlainCallback txDoneCallback(&txDone);
+
+static uint8_t radio_buffer[PAYLOAD_LENGTH];
+static uint8_t* radio_ptr = radio_buffer;
+static uint8_t  radio_len = sizeof(radio_buffer);
+static int8_t rssi;
+static uint8_t lqi;
+static uint8_t crc;
+
+static Serial serial(uart);
 
 /*================================= public ==================================*/
 
-int main(void)
+int main (void)
 {
-    bool status;
+    // Initialize board
+    board.init();
 
-	// Initialize board
-	board.init();
-
-    // Enable the SPI peripheral
+    // Enable the SPI interface
     spi.enable(SPI_BAUDRATE);
 
     // Set radio callbacks
-    at86rf215.setCallbacks(&rf09Callback_, &rf24Callback_);
+    at86rf215.setTxCallbacks(RADIO_CORE, &txInitCallback, &txDoneCallback);
+    at86rf215.setRxCallbacks(RADIO_CORE, &rxInitCallback, &rxDoneCallback);
     at86rf215.enableInterrupts();
 
-    // Enable general interrupts
-    board.enableInterrupts();
+    // Turn AT86RF215 radio on
+    at86rf215.on();
 
-    // Forever and ever!
-    while (true) {
-        
-        at86rf215.enable();
+    // Create the blink task
+    xTaskCreate(prvGreenLedTask, (const char *) "Green", 128, NULL, GREEN_LED_TASK_PRIORITY, NULL);
 
-        board.delayMilliseconds(1000);
-        
-        status = at86rf215.check();
+#if (RADIO_MODE == RADIO_MODE_RX)
+    // Enable the UART driver
+    uart.enable(UART_BAUDRATE);
 
-        if (status) {
-            led_green.on();
-        } else {
-            led_red.on();
-        }
+    // Create the radio receive task
+    xTaskCreate(prvRadioRxTask, (const char *) "RadioRx", 128, NULL, RADIO_RX_TASK_PRIORITY, NULL);
+#elif (RADIO_MODE == RADIO_MODE_TX)
+    // Create the radio transmit task
+    xTaskCreate(prvRadioTxTask, (const char *) "RadioTx", 128, NULL, RADIO_TX_TASK_PRIORITY, NULL);
+#endif
 
-        board.delayMilliseconds(1000);
-
-        at86rf215.sleep();
-
-        led_green.off();
-        led_orange.off();
-        led_yellow.off();
-        led_red.off();
-
-        board.delayMilliseconds(1000);
-    }
-    	
+    // Start the scheduler
+    Scheduler::run();
 }
 
 /*================================ private ==================================*/
 
-static void rf09Callback(void)
+static void prvGreenLedTask(void *pvParameters)
 {
-    led_yellow.on();
+    // Forever
+    while (true)
+    {
+        // Turn off the green LED and keep it for 950 ms
+        led_green.off();
+        vTaskDelay(950 / portTICK_RATE_MS);
+
+        // Turn on the green LED and keep it for 50 ms
+        led_green.on();
+        vTaskDelay(50 / portTICK_RATE_MS);
+    }
 }
 
-static void rf24Callback(void)
+static void prvRadioRxTask(void *pvParameters)
 {
-    led_orange.on();
+    static At86rf215::RadioResult result;
+
+    // Forever
+    while (true)
+    {
+        // Put the radio transceiver in receive mode
+        at86rf215.ready(RADIO_CORE);
+        at86rf215.receive(RADIO_CORE);
+
+        // Turn the yellow LED on when a the radio is receiving
+        led_yellow.on();
+
+        // Take the rxSemaphre, block until available
+        if (rxSemaphore.take())
+        {
+            // Turn the yellow LED off when a packet is received
+            led_yellow.off();
+
+            // Get a packet from the radio buffer
+            radio_ptr = radio_buffer;
+            radio_len = sizeof(radio_buffer);
+            result = at86rf215.getPacket(RADIO_CORE, radio_ptr, &radio_len, &rssi, &lqi, &crc);
+
+            if (result == At86rf215::Success && crc)
+            {
+                uart.writeByte(rssi);
+                uart.writeByte(crc >> 7);
+            }
+        }
+    }
+}
+
+static void prvRadioTxTask(void *pvParameters)
+{
+    static At86rf215::RadioResult result;
+
+    // Get the EUI64 address of the board
+    board.getEUI48(radio_buffer);
+
+    // Forever
+    while (true)
+    {
+        // Take the txSemaphre, block until available
+        if (txSemaphore.take())
+        {
+            // Turn on the radio transceiver
+            at86rf215.ready(RADIO_CORE);
+
+            // Turn the yellow LED on when the packet is being loaded
+            led_yellow.on();
+
+            // Load the EUI64 address to the transmit buffer
+            radio_ptr = radio_buffer;
+            radio_len = EUI48_LENGTH;
+            result = at86rf215.loadPacket(RADIO_CORE, radio_ptr, radio_len);
+
+            if (result == At86rf215::Success)
+            {
+                // Put the radio transceiver in transmit mode
+                at86rf215.transmit(RADIO_CORE);
+
+                // Turn the yellow LED off when the packet has beed loaded
+                led_yellow.off();
+            }
+
+            // Delay the transmission of the next packet 250 ms
+            vTaskDelay(250 / portTICK_RATE_MS);
+        }
+    }
+}
+
+static void rxInit(void)
+{
+    // Turn on the radio LED as the radio is now receiving a packet
+    led_red.on();
+}
+
+static void rxDone(void)
+{
+    // Turn off the radio LED as the packet is received
+    led_red.off();
+
+    // Let the task run once a packet is received
+    rxSemaphore.giveFromInterrupt();
+}
+
+static void txInit(void)
+{
+    // Turn on the radio LED as the packet is now transmitting
+    led_red.on();
+}
+
+static void txDone(void)
+{
+    // Turn off the radio LED as the packet is transmitted
+    led_red.off();
+
+    // Let the task run once a packet is transmitted
+    txSemaphore.giveFromInterrupt();
 }
