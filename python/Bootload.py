a06ddeb3b6ee074f07a9bb2b92a28b9d44131d3a
Pere Tuset <peretuset@gmail.com>
2018-08-30 01:04:07 +0200

    Add Bootload class to allow for automatic bootloading with Python scripts.

---
+++ python/Bootload.py
@@ -0,0 +1,42 @@
+'''
+@file       Bootload.py
+@author     Pere Tuset-Peiro  (peretuset@openmote.com)
+@version    v0.1
+@date       May, 2015
+@brief      
+
+@copyright  Copyright 2015, OpenMote Technologies, S.L.
+            This file is licensed under the GNU General Public License v2.
+'''
+
+import logging
+import os
+import subprocess
+
+# Import logging configuration
+logger = logging.getLogger(__name__)
+
+class Bootload(object):
+    def __init__(self, serial_name = None, serial_speed = 115200, image = None):
+        self.python   = "python"
+        self.bsl_path = os.path.join("..", "..", "tools", "cc2538-bsl")
+        self.bsl_file = "cc2538-bsl.py"
+
+        self.serial_name = serial_name
+        self.serial_speed = serial_speed
+        self.image = image
+
+        self.bsl_params = "-e -w --bootloader-invert-lines -p {} -b {} {}".format(self.serial_name, self.serial_speed, self.image)
+
+        self.script_cmd = os.path.join(self.bsl_path, self.bsl_file)
+        self.script_args = self.bsl_params
+
+    def start(self):
+        try:
+            cmd = self.python + " " + self.script_cmd + " " + self.script_args
+            status = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, check=True) 
+            return True
+        except:
+            logger.error("start: Executing the bootload process.")
+        
+        return False
