c4b41462171b6805c78390ebb86d1cc8c74b4620
Pere Tuset <peretuset@gmail.com>
2019-02-07 09:27:53 +0100

    Updated test-hdlc project.

--- test/test-hdlc/src/main.cpp
+++ test/test-hdlc/src/main.cpp
@@ -11,22 +11,17 @@
 
 /*================================ include ==================================*/
 
-#include "FreeRTOS.h"
-#include "task.h"
+#include "BoardImplementation.hpp"
 
-#include "board.h"
-
-#include "Serial.h"
-
-#include "Scheduler.h"
-#include "Task.h"
+#include "Callback.hpp"
+#include "Scheduler.hpp"
+#include "Semaphore.hpp"
+#include "Task.hpp"
 
 /*================================ define ===================================*/
 
 #define GREEN_LED_TASK_PRIORITY             ( tskIDLE_PRIORITY + 0 )
-#define SERIAL_TASK_PRIORITY                ( tskIDLE_PRIORITY + 1 )
-
-#define UART_BAUDRATE                       ( 115200 )
+#define HDLC_TASK_PRIORITY                  ( tskIDLE_PRIORITY + 1 )
 
 /*================================ typedef ==================================*/
 
@@ -37,84 +32,43 @@ static void prvSerialTask(void *pvParameters);
 
 /*=============================== variables =================================*/
 
-static uint8_t serial_tx_buffer[] = {'O','p','e','n','M','o','t','e','-','C','C','2','5','3','8'};
-static uint8_t* serial_tx_ptr = serial_tx_buffer;
-static uint8_t serial_tx_len  = sizeof(serial_tx_buffer);
-
-static uint8_t serial_rx_buffer[128];
-static uint8_t* serial_rx_ptr = serial_rx_buffer;
-static uint8_t serial_rx_len  = sizeof(serial_rx_buffer);
-
-static Serial serial(uart);
+static Task heartbeatTask{(const char *) "Green", 128, GREEN_LED_TASK_PRIORITY, prvGreenLedTask, nullptr};
+static Task hdlcTask{(const char *) "Hdlc", 128, HDLC_TASK_PRIORITY, prvHdlcTask, nullptr};
 
 /*================================= public ==================================*/
 
 int main (void)
 {
-    // Initialize board
-    board.init();
+  /* Initialize board */
+  board.init();
 
-    // Enable the UART peripheral and the serial driver
-    uart.enable(UART_BAUDRATE);
-    serial.init();
-
-    // Create two FreeRTOS tasks
-    xTaskCreate(prvGreenLedTask, (const char *) "Green", 128, NULL, GREEN_LED_TASK_PRIORITY, NULL);
-    xTaskCreate(prvSerialTask, (const char *) "Serial", 128, NULL, SERIAL_TASK_PRIORITY, NULL);
-
-    // Start the scheduler
-    Scheduler::run();
+  /* Start the scheduler */
+  Scheduler::run();
 }
 
 /*=============================== protected =================================*/
 
 /*================================ private ==================================*/
 
-static void prvSerialTask(void *pvParameters)
+static void prvHdlcTask(void *pvParameters)
 {
-    // Turn on red LED
-    led_red.on();
-
-    // Print buffer via Serial/UART
-    serial.write(serial_tx_ptr, serial_tx_len);
-
-    // Turn off red LED
-    led_red.off();
-
-    // Forever
-    while (true)
-    {
-        serial_rx_ptr = serial_rx_buffer;
-        serial_rx_len = sizeof(serial_rx_buffer);
-
-        // Read buffer via Serial/UART
-        serial_rx_len = serial.read(serial_rx_ptr, serial_rx_len);
-
-        // Delay for 250 ms
-        Task::delay(250);
-
-        // Turn on red LED
-        led_yellow.on();
-
-        // Write buffer via Serial/UART
-        serial.write(serial_rx_ptr, serial_rx_len);
-
-        // Turn off red LED
-        led_yellow.off();
-    }
+  /* Forever */
+  while (true)
+  {
+  }
 }
 
 static void prvGreenLedTask(void *pvParameters)
 {
-    // Forever
-    while (true)
-    {
-        // Turn off green LED for 950 ms
-        led_green.off();
-        Task::delay(950);
+  /* Forever */
+  while (true)
+  {
+    /* Turn off green LED for 990 ms */
+    led_green.off();
+    Scheduler::delay_ms(990);
 
-        // Turn on green LED for 50 ms
-        led_green.on();
-        Task::delay(50);
-    }
+    /* Turn on green LED for 10 ms */
+    led_green.on();
+    Scheduler::delay_ms(10);
+  }
 }
