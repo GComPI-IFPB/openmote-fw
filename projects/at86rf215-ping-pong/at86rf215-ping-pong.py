79ccb630978887a2faa64a3356c81f563b99e767
Pere Tuset <peretuset@gmail.com>
2019-02-14 09:41:45 +0100

    Add at86rf215-ping-pong project.

---
+++ projects/at86rf215-ping-pong/at86rf215-ping-pong.py
@@ -0,0 +1,232 @@
+#!/usr/bin/python
+
+'''
+@file       at86rf215-ping-pong.py
+@author     Pere Tuset-Peiro  (peretuset@openmote.com)
+@version    v0.1
+@date       February, 2019
+@brief      
+
+@copyright  Copyright 2019, OpenMote Technologies, S.L.
+            This file is licensed under the GNU General Public License v2.
+'''
+
+import os
+import sys
+pwd = os.path.abspath(__file__)
+pwd = os.path.dirname(os.path.dirname(os.path.dirname(pwd)))
+pwd = os.path.join(pwd, 'python')
+sys.path.append(pwd)
+
+import argparse
+import signal
+import struct
+import time
+import random
+import string
+
+import Serial
+
+finished = False
+
+timeout = 0.001
+
+def signal_handler(sig, frame):
+    global finished
+    finished = True
+
+def ping(port = None, baudrate = None, uncertain = None, length = None, sleep = None, duration = None, report = None):
+    global finished
+
+    # Calculate duration in miliseconds
+    if (duration > 0):
+        duration = 1000 * duration
+    else:
+        duration = -1
+
+    # Generate random message
+    if (uncertain == True):
+        message     = "".join(random.choice(string.ascii_letters) for x in range(length))
+    else:
+        message     = "".join("a" for x in range(length))
+    tx_length = len(message)
+
+    # Create and start Serial manager
+    serial = Serial.Serial(name = port, baudrate = baudrate, timeout = timeout)
+    serial.start()
+
+    print("Starting ping at port {} with {} bauds, message length {} bytes and {} sleep for {} ms.".format(port, baudrate, tx_length, sleep, duration))
+    print("Message={}, length={}".format(message, tx_length))
+
+    # Prepare statistics
+    bytes_sent       = 0
+    bytes_received   = 0
+    packets_sent     = 0
+    packets_received = 0
+
+    # Store start time
+    start_time = time.time()
+    total_time = 0
+
+    # Transmit message 
+    serial.transmit(message)
+
+    # Account for sent packets and bytes
+    bytes_sent += tx_length
+    packets_sent += 1
+
+    # Repeat until finish condition
+    while (not finished):
+        # Try to receive a Serial message
+        received, rx_length = serial.receive(timeout = timeout)
+
+        # If we received a message
+        if (rx_length > 0):
+            # Convert message to string
+            received = [chr(c) for c in received]
+
+            # Account for received packets and bytes
+            bytes_received += rx_length
+            packets_received += 1
+
+            # Generate a new random message to be sent
+            if (uncertain == True):
+                message = "".join(random.choice(string.ascii_letters) for x in range(tx_length))
+
+            # Transmit message 
+            serial.transmit(message)
+
+            # Account for sent messages
+            bytes_sent += tx_length
+            packets_sent += 1
+        
+        # If sleep
+        if (sleep > 0.001):
+            time.sleep(sleep)
+
+        # Calculate elapsed time
+        current_time = time.time()
+        elapsed_time = 1000 * (current_time - start_time)
+
+        # If elapsed time is above report
+        if (elapsed_time > report):
+            tx_bandwidth = bytes_sent * 8 / elapsed_time
+            rx_bandwidth = bytes_received * 8 / elapsed_time
+            print("Sent {} packets ({} bytes) and received {} packets ({} bytes) in the last {:.2f} ms ({:.2f}/{:.2f} kbps)".format(packets_sent, bytes_sent,
+                                                                                                                                 packets_received, bytes_received,
+                                                                                                                                 elapsed_time, tx_bandwidth, rx_bandwidth))
+
+            # Restore start time
+            start_time = current_time
+
+            # Restore statistics
+            packets_sent     = 0
+            packets_received = 0
+            bytes_sent       = 0
+            bytes_received   = 0
+
+            # Account for elapsed time
+            total_time  += elapsed_time
+
+        # If total time is above duration
+        if (duration > 0 and total_time > duration):
+            # Signal the script to finish
+            finished = True
+    
+    # Stop the serial port
+    serial.stop()
+
+def pong(port = None, baudrate = None, length = None, report = None):
+    global finished
+
+    # Create and start Serial manager
+    serial = Serial.Serial(name = port, baudrate = baudrate, timeout = timeout)
+    serial.start()
+
+    print("Starting pong at port {} with bauds {}.".format(port, baudrate))
+    
+    # Prepare statistics
+    bytes_sent       = 0
+    bytes_received   = 0
+    packets_sent     = 0
+    packets_received = 0
+
+    # Store start time
+    start_time = time.time()
+    total_time = 0
+
+    # Repeat until finish condition
+    while (not finished):
+        # Try to receive a Serial message
+        received, rx_length = serial.receive(timeout = timeout)
+
+        # If we received a message
+        if (rx_length > 0):
+            # Convert message to string
+            received  = [chr(c) for c in received]
+            rx_length = len(received)
+
+            # Account for received messages
+            bytes_received   += rx_length
+            packets_received += 1
+
+            # Transmit message 
+            serial.transmit(received)
+
+            # Account for sent messages
+            bytes_sent   += rx_length
+            packets_sent += 1
+        
+        # Calculate elapsed time
+        current_time = time.time()
+        elapsed_time = 1000 * (current_time - start_time)
+
+        # If elapsed time is above report time
+        if (elapsed_time > report):
+            tx_bandwidth = bytes_sent * 8 / elapsed_time
+            rx_bandwidth = bytes_received * 8 / elapsed_time
+            print("Sent {} packets ({} bytes) and received {} packets ({} bytes) in the last {:.2f} ms ({:.2f}/{:.2f} kbps)".format(packets_sent, bytes_sent,
+                                                                                                                                 packets_received, bytes_received,
+                                                                                                                                 elapsed_time, tx_bandwidth, rx_bandwidth))
+
+            # Restore start time
+            start_time = current_time
+
+            # Restore statistics
+            packets_sent     = 0
+            packets_received = 0
+            bytes_sent       = 0
+            bytes_received   = 0
+
+            # Account for elapsed
+            total_time  += elapsed_time
+    
+    # Stop the serial port
+    serial.stop()
+
+def main():
+    # Set up SIGINT signal
+    signal.signal(signal.SIGINT, signal_handler)
+
+    # Create argument parser
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("-m", "--mode", choices=["ping", "pong"], type=str, default="ping")
+    parser.add_argument("-p", "--port", type=str, required=True)
+    parser.add_argument("-b", "--baudrate", type=int, default=115200)
+    parser.add_argument("-l", "--length", type=int, default=100)
+    parser.add_argument("-d", "--duration", type=int, default=0)
+    parser.add_argument("-r", "--report", type=int, default=1000)
+    parser.add_argument("-s", "--sleep", type=float, default=0.1)
+    parser.add_argument("-u", "--uncertain", type=lambda s: (str(s).lower() in ['true', 'on', 'yes', '1']), default=False)
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Execute program
+    if (args.mode == "ping"):
+        ping(port = args.port, baudrate=args.baudrate, uncertain=args.uncertain, length=args.length, sleep=args.sleep, duration=args.duration, report=args.report)
+    elif (args.mode == "pong"):
+        pong(port = args.port, baudrate=args.baudrate, length=args.length, report=args.report)
+    
+if __name__ == "__main__":
+    main()
---
+++ projects/at86rf215-ping-pong/src/main.cpp
@@ -0,0 +1,284 @@
+/**
+ * @file       main.cpp
+ * @author     Pere Tuset-Peiro (peretuset@openmote.com)
+ * @version    v0.1
+ * @date       February, 2019
+ * @brief
+ *
+ * @copyright  Copyright 2019, OpenMote Technologies, S.L.
+ *             This file is licensed under the GNU General Public License v2.
+ */
+
+/*================================ include ==================================*/
+
+#include "BoardImplementation.hpp"
+
+#include "Gpio.hpp"
+
+#include "Callback.hpp"
+#include "Scheduler.hpp"
+#include "Semaphore.hpp"
+#include "Task.hpp"
+
+#include "Serial.hpp"
+
+#include "At86rf215.hpp"
+#include "At86rf215_conf.h"
+
+/*================================ define ===================================*/
+
+#define GREEN_LED_TASK_PRIORITY             ( tskIDLE_PRIORITY + 0 )
+#define RADIO_TASK_PRIORITY                 ( tskIDLE_PRIORITY + 1 )
+#define SERIAL_TASK_PRIORITY                ( tskIDLE_PRIORITY + 2 )
+
+#define UART_BAUDRATE                       ( 921600 )
+#define SPI_BAUDRATE                        ( 16000000 )
+
+#define BUFFER_LENGTH                       ( 1024 )
+
+/*================================ typedef ==================================*/
+
+/*=============================== prototypes ================================*/
+
+static void prvGreenLedTask(void *pvParameters);
+static void prvRadioTask(void *pvParameters);
+static void prvSerialTask(void *pvParameters);
+
+static void radio_rx_init(void);
+static void radio_rx_done(void);
+static void radio_tx_init(void);
+static void radio_tx_done(void);
+
+/*=============================== variables =================================*/
+
+static Serial serial(uart);
+
+static Task heartbeatTask{(const char *) "Green", 128, GREEN_LED_TASK_PRIORITY, prvGreenLedTask, nullptr};
+static Task radioTask{(const char *) "Radio", 128, RADIO_TASK_PRIORITY, prvRadioTask, nullptr};
+static Task serialTask{(const char *) "Serial", 128, SERIAL_TASK_PRIORITY, prvSerialTask, nullptr};
+
+static PlainCallback radio_rx_init_cb(&radio_rx_init);
+static PlainCallback radio_rx_done_cb(&radio_rx_done);
+static PlainCallback radio_tx_init_cb(&radio_tx_init);
+static PlainCallback radio_tx_done_cb(&radio_tx_done);
+
+static SemaphoreBinary tx_semaphore(false);
+static SemaphoreBinary rx_semaphore(false);
+
+static uint8_t serial_rx_buffer[BUFFER_LENGTH];
+static uint16_t serial_rx_buffer_len = sizeof(serial_rx_buffer);
+
+static uint8_t radio_rx_buffer[BUFFER_LENGTH];
+static uint16_t radio_rx_buffer_len = sizeof(radio_rx_buffer);
+
+static uint8_t radio_tx_buffer[BUFFER_LENGTH];
+static uint16_t radio_tx_buffer_len = sizeof(radio_tx_buffer);
+
+static bool received = false;
+static bool transmit = false;
+
+/*================================= public ==================================*/
+
+int main(void)
+{
+  /* Initialize the board */
+  board.init();
+  
+  /* Enable the SPI interface */
+  spi.enable(SPI_BAUDRATE);
+  
+  /* Enable the UART interface */
+  uart.enable(UART_BAUDRATE);
+  
+  /* Initialize Serial interface */
+  serial.init();
+
+  /* Start the scheduler */
+  Scheduler::run();
+}
+
+/*================================ private ==================================*/
+
+static void prvRadioTask(void *pvParameters)
+{
+  bool status; 
+
+  /* Turn AT86RF215 radio on */
+  at86rf215.on();
+  
+  /* Check AT86RF215 radio */
+  status = at86rf215.check();
+  if (!status)
+  {
+    /* Blink red LED */ 
+    board.error();
+  }
+  
+  /* Set radio callbacks and enable interrupts */
+  at86rf215.setTxCallbacks(At86rf215::CORE_RF09, &radio_tx_init_cb, &radio_tx_done_cb);
+  at86rf215.setRxCallbacks(At86rf215::CORE_RF09, &radio_rx_init_cb, &radio_rx_done_cb);
+  at86rf215.enableInterrupts();
+  
+  /* Wake up and configure radio */
+  at86rf215.wakeup(At86rf215::CORE_RF09);
+  at86rf215.configure(At86rf215::CORE_RF09,
+                      &radio_settings[CONFIG_OFDM_2_MCS_5],
+                      &frequency_settings[FREQUENCY_OFDM_2]);
+
+  /* Ready to transmit */
+  at86rf215.ready(At86rf215::CORE_RF09);
+  
+  /* Forever */
+  while (true)
+  {
+    At86rf215::RadioResult result;
+
+    /* We don't have anything to trasnmit, so receive */
+    if (!transmit)
+    {
+      int8_t rssi, lqi;
+      bool taken;
+      bool crc;
+
+      /* Initialize packet pointer and length */
+      uint8_t* buffer_ptr = radio_rx_buffer;
+      uint16_t buffer_len = radio_rx_buffer_len;
+
+      /* Transmit packet */
+      at86rf215.receive(At86rf215::CORE_RF09);
+      
+      /* Wait until packet has been received */
+      taken = rx_semaphore.take();
+      
+      /* If we have received a packet */
+      if (taken == true && received == true)
+      {
+        /* Load packet to radio */
+        result = at86rf215.getPacket(At86rf215::CORE_RF09, buffer_ptr, &buffer_len, &rssi, &lqi, &crc);
+        
+        /* Check packet has been received successfully */
+        if (result == RadioResult::RadioResult_Success && crc == true)
+        {
+          /* Write radio buffer via Serial */
+          serial.write(buffer_ptr, buffer_len, true);
+        }
+      }
+      else
+      {
+        led_yellow.off();
+      }
+      
+      /* Go back to ready */
+      at86rf215.ready(At86rf215::CORE_RF09);
+    }
+    /* We got a packet to transmit, fire it! */
+    else
+    {
+      bool sent;
+      
+      /* Initialize packet pointer and length */
+      uint8_t* buffer_ptr = radio_tx_buffer;
+      uint16_t buffer_len = radio_tx_buffer_len;
+
+      /* Load packet to radio */
+      at86rf215.loadPacket(At86rf215::CORE_RF09, buffer_ptr, buffer_len);
+      
+      /* Transmit packet */
+      at86rf215.transmit(At86rf215::CORE_RF09);
+      
+      /* Wait until packet has been transmitted */
+      sent = tx_semaphore.take();
+      if (sent == true)
+      {
+        led_red.on();
+        Scheduler::delay_ms(1);
+        led_red.off();
+      }
+      
+      /* Go back to ready */
+      at86rf215.ready(At86rf215::CORE_RF09);
+
+      transmit = false;
+    }
+  }
+  
+  /* Turn AT86RF215 radio off */
+  at86rf215.off();
+}
+
+static void prvSerialTask(void *pvParameters)
+{
+  /* Forever */
+  while (true)
+  {
+    /* Restore serial_rx pointer and length */
+    uint8_t* serial_rx_ptr = serial_rx_buffer;
+    uint16_t serial_rx_len = sizeof(serial_rx_buffer);
+
+    /* Read buffer using Serial */
+    serial_rx_len = serial.read(serial_rx_ptr, serial_rx_len);
+    
+    /* If we have received a message */
+    if (serial_rx_len > 0)
+    {
+      /* Copy packet to radio transmit buffer */
+      dma.memcpy(radio_tx_buffer, serial_rx_ptr, serial_rx_len);
+      radio_tx_buffer_len = serial_rx_len;
+        
+      /* Mark that we have not received, but we need to transmit */
+      received = false;
+      transmit = true;
+      
+      /* Notify radio to transmit */
+      rx_semaphore.give();
+    }
+  }
+}
+
+static void prvGreenLedTask(void *pvParameters)
+{
+  /* Forever */
+  while (true)
+  {
+    /* Turn on green LED for 100 ms */
+    led_green.on();
+    Scheduler::delay_ms(100);
+    
+    /* Turn off green LED for 900 ms */
+    led_green.off();
+    Scheduler::delay_ms(900);
+  }
+}
+
+static void radio_tx_init(void)
+{
+  /* Turn on orange LED */
+  led_orange.on();
+}
+
+static void radio_tx_done(void)
+{
+  /* Turn off orange LED */
+  led_orange.off();
+
+  /* Notify we have transmitted a packet */
+  tx_semaphore.giveFromInterrupt();
+}
+
+static void radio_rx_init(void)
+{
+  /* Turn on yellow LED */
+  led_yellow.on();
+}
+
+static void radio_rx_done(void)
+{
+  /* Turn off yellow LED */
+  led_yellow.off();
+  
+  /* Mark that we have received a packet */
+  received = true;
+
+  /* Notify we have received a packet */
+  rx_semaphore.giveFromInterrupt();
+}
