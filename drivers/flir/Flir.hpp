b75cd5405758b86d64e93a2e26edb0c2c84512c5
Pere Tuset <peretuset@gmail.com>
2019-03-01 20:12:11 +0100

    Add Flir drivers.

---
+++ drivers/flir/Flir.hpp
@@ -0,0 +1,42 @@
+/**
+ * @file       Flir.hpp
+ * @author     Pere Tuset-Peiro (peretuset@openmote.com)
+ * @version    v0.1
+ * @date       February, 2019
+ * @brief
+ *
+ * @copyright  Copyright 2019, OpenMote Technologies, S.L.
+ *             This file is licensed under the GNU General Public License v2.
+ */
+
+#ifndef FLIR_HPP_
+#define FLIR_HPP_
+
+#include "Gpio.hpp"
+#include "Spi.hpp"
+#include "Callback.hpp"
+
+struct FlirHeader
+{
+  uint8_t packet_id;
+  uint8_t camera_id;
+  uint16_t crc;
+};
+
+class Flir
+{
+public:
+	Flir(Spi& spi, GpioOut& cs);
+  void init(void);
+  void wakeup(void);
+  bool readFirst(uint8_t* buffer, uint16_t length);
+  void readRemaining(uint8_t* buffer, uint16_t length);
+private:
+  bool readPacket(uint8_t* buffer_payload, uint16_t length);
+  bool isDiscardPacket(void);
+private:
+	Spi& spi_;
+  GpioOut& cs_;
+};
+
+#endif /* FLIR_HPP_ */
---
+++ drivers/flir/Flir_regs.h
@@ -0,0 +1,21 @@
+/**
+ * @file       Flir_regs.h
+ * @author     Pere Tuset-Peiro     (peretuset@openmote.com)
+ * @version    v0.1
+ * @date       February, 2019
+ * @brief
+ *
+ * @copyright  Copyright 2019, OpenMote Technologies, S.L.
+ *             This file is licensed under the GNU General Public License v2.
+ */
+
+#ifndef FLIR_REGS_H_
+#define FLIR_REGS_H_
+
+#define FLIR_HEADER_SIZE        ( 4 )
+#define FLIR_DATA_SIZE          ( 160 )
+
+#define FLIR_ROW_SIZE           ( 60 )
+#define FLIR_COL_SIZE           ( 80 )
+
+#endif /* FLIR_REGS_H_ */
